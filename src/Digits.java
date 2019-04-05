b7ade7c4eefc5ba283dfd550a6e554f00ac12a17
BT_1N3_12 <sbajnath6151@bths.edu>
2019-04-05 08:36:28 -0400

    s

--- src/Digits.java
+++ src/Digits.java
@@ -10,11 +10,19 @@ public class Digits
     //represents num
     public Digits (int num)
     {
+        List<Integer> digitList = getDigitList();
         //implemented in part (a)
+        String numStr = ""+num;
+        for(int i = 0; i < numStr.length(); i++)
+        {
+            int result = Integer.parseInt(numStr.substring(i,i+1));
+            digitList.add(result);
+        }
     }
     public boolean isStrictlyIncreasing()
     {
         //implemented in part (b)
+        return true;
     }
     //Extra method to make the runner easier to read
     public ArrayList<Integer> getDigitList()
